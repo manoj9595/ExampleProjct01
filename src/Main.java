de1ccb7c476016047d69c80d63e35f6c6fd5ac5b
manoj <manoj.bobby796@gmail.com>
2019-09-05 11:51:34 -0400

    added a feature

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,12 @@ public class Main {
 		Scanner keyboard = new Scanner(System.in);
 		String personName = keyboard.nextLine();
 		System.out.println("Hello " +  personName);
+		
+		System.out.println("How old are you?");
+		int age = keyboard.nextInt();
+		int futureAge = age + 10;
+		System.out.println("in 10 year you will be:" + futureAge);
+		
 	}
 
 }
